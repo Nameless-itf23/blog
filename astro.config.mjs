7d69a9f1eebd59b3ac6e1ca76a197d5955a2b7c2
Nameless-itf23 <ohutoncat@gmail.com>
2024-02-14 16:03:59 +0900

    feat: dracula theme

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,14 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()]
+  integrations: [mdx()],
+  markdown: {
+    shikiConfig: {
+      // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+      theme: 'dracula',
+      // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+      langs: [],
+      wrap: true,
+    },
+  }
 });
